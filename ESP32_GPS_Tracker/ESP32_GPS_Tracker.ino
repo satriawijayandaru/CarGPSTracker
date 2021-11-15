6408160f67fb67e6d90a5e004eb61af4e4807bfd
vrey11 <yanda1998@gmail.com>
2021-11-15 20:29:23 +0700

    wifi connection working

--- ESP32_GPS_Tracker/ESP32_GPS_Tracker.ino
+++ ESP32_GPS_Tracker/ESP32_GPS_Tracker.ino
@@ -1,20 +1,39 @@
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <WiFi.h>
 
+int showDebug = 1;
+
+//WIFI CONFIGURATION
+const char* ssid     = "XDA";
+const char* password = "namaguee";
+
+//GPS CONFIGURATION
 static const uint32_t GPSBaud = 9600;
-
 // The TinyGPS++ object
 TinyGPSPlus gps;
-
-
-// The serial connection to the GPS device
-SoftwareSerial ss(14, 15);
-//#define ss Serial2
+SoftwareSerial ss(14, 15);      //USING SOFTWARE SERIAL
+//#define ss Serial2            //USING HARDWARE SERIAL (PREFERRED IF POSSIBLE)
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
-  Serial.println("GPS START");
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Connecting to GPS Module");
+  Serial.print("Baud Rate = ");
+  Serial.println(GPSBaud);
   ss.begin(GPSBaud);
   //  changeFrequency();
   //  delay(100);
@@ -22,6 +41,7 @@ void setup() {
 }
 
 void loop() {
+  WiFiClient client;
   GPSgetData();
 }
 
@@ -30,105 +50,107 @@ void GPSgetData() {
   while (ss.available() > 0) {
     gps.encode(ss.read());
     if (gps.location.isUpdated()) {
-      // Latitude in degrees (double)
-      Serial.print("Latitude= ");
-      Serial.print(gps.location.lat(), 6);
-      // Longitude in degrees (double)
-      Serial.print(" Longitude= ");
-      Serial.println(gps.location.lng(), 6);
+      if (showDebug == 1) {
+        // Latitude in degrees (double)
+        Serial.print("Latitude= ");
+        Serial.print(gps.location.lat(), 6);
+        // Longitude in degrees (double)
+        Serial.print(" Longitude= ");
+        Serial.println(gps.location.lng(), 6);
 
-      // Raw latitude in whole degrees
-      Serial.print("Raw latitude = ");
-      Serial.print(gps.location.rawLat().negative ? "-" : "+");
-      Serial.println(gps.location.rawLat().deg);
-      // ... and billionths (u16/u32)
-      Serial.println(gps.location.rawLat().billionths);
+        // Raw latitude in whole degrees
+        Serial.print("Raw latitude = ");
+        Serial.print(gps.location.rawLat().negative ? "-" : "+");
+        Serial.println(gps.location.rawLat().deg);
+        // ... and billionths (u16/u32)
+        Serial.println(gps.location.rawLat().billionths);
 
-      // Raw longitude in whole degrees
-      Serial.print("Raw longitude = ");
-      Serial.print(gps.location.rawLng().negative ? "-" : "+");
-      Serial.println(gps.location.rawLng().deg);
-      // ... and billionths (u16/u32)
-      Serial.println(gps.location.rawLng().billionths);
+        // Raw longitude in whole degrees
+        Serial.print("Raw longitude = ");
+        Serial.print(gps.location.rawLng().negative ? "-" : "+");
+        Serial.println(gps.location.rawLng().deg);
+        // ... and billionths (u16/u32)
+        Serial.println(gps.location.rawLng().billionths);
 
-      // Raw date in DDMMYY format (u32)
-      Serial.print("Raw date DDMMYY = ");
-      Serial.println(gps.date.value());
+        // Raw date in DDMMYY format (u32)
+        Serial.print("Raw date DDMMYY = ");
+        Serial.println(gps.date.value());
 
-      // Year (2000+) (u16)
-      Serial.print("Year = ");
-      Serial.println(gps.date.year());
-      // Month (1-12) (u8)
-      Serial.print("Month = ");
-      Serial.println(gps.date.month());
-      // Day (1-31) (u8)
-      Serial.print("Day = ");
-      Serial.println(gps.date.day());
+        // Year (2000+) (u16)
+        Serial.print("Year = ");
+        Serial.println(gps.date.year());
+        // Month (1-12) (u8)
+        Serial.print("Month = ");
+        Serial.println(gps.date.month());
+        // Day (1-31) (u8)
+        Serial.print("Day = ");
+        Serial.println(gps.date.day());
 
-      // Raw time in HHMMSSCC format (u32)
-      Serial.print("Raw time in HHMMSSCC = ");
-      Serial.println(gps.time.value());
+        // Raw time in HHMMSSCC format (u32)
+        Serial.print("Raw time in HHMMSSCC = ");
+        Serial.println(gps.time.value());
 
-      // Hour (0-23) (u8)
-      Serial.print("Hour = ");
-      Serial.println(gps.time.hour());
-      // Minute (0-59) (u8)
-      Serial.print("Minute = ");
-      Serial.println(gps.time.minute());
-      // Second (0-59) (u8)
-      Serial.print("Second = ");
-      Serial.println(gps.time.second());
-      // 100ths of a second (0-99) (u8)
-      Serial.print("Centisecond = ");
-      Serial.println(gps.time.centisecond());
+        // Hour (0-23) (u8)
+        Serial.print("Hour = ");
+        Serial.println(gps.time.hour());
+        // Minute (0-59) (u8)
+        Serial.print("Minute = ");
+        Serial.println(gps.time.minute());
+        // Second (0-59) (u8)
+        Serial.print("Second = ");
+        Serial.println(gps.time.second());
+        // 100ths of a second (0-99) (u8)
+        Serial.print("Centisecond = ");
+        Serial.println(gps.time.centisecond());
 
-      // Raw speed in 100ths of a knot (i32)
-      Serial.print("Raw speed in 100ths/knot = ");
-      Serial.println(gps.speed.value());
-      // Speed in knots (double)
-      Serial.print("Speed in knots/h = ");
-      Serial.println(gps.speed.knots());
-      // Speed in miles per hour (double)
-      Serial.print("Speed in miles/h = ");
-      Serial.println(gps.speed.mph());
-      // Speed in meters per second (double)
-      Serial.print("Speed in m/s = ");
-      Serial.println(gps.speed.mps());
-      // Speed in kilometers per hour (double)
-      Serial.print("Speed in km/h = ");
-      Serial.println(gps.speed.kmph());
+        // Raw speed in 100ths of a knot (i32)
+        Serial.print("Raw speed in 100ths/knot = ");
+        Serial.println(gps.speed.value());
+        // Speed in knots (double)
+        Serial.print("Speed in knots/h = ");
+        Serial.println(gps.speed.knots());
+        // Speed in miles per hour (double)
+        Serial.print("Speed in miles/h = ");
+        Serial.println(gps.speed.mph());
+        // Speed in meters per second (double)
+        Serial.print("Speed in m/s = ");
+        Serial.println(gps.speed.mps());
+        // Speed in kilometers per hour (double)
+        Serial.print("Speed in km/h = ");
+        Serial.println(gps.speed.kmph());
 
-      // Raw course in 100ths of a degree (i32)
-      Serial.print("Raw course in degrees = ");
-      Serial.println(gps.course.value());
-      // Course in degrees (double)
-      Serial.print("Course in degrees = ");
-      Serial.println(gps.course.deg());
+        // Raw course in 100ths of a degree (i32)
+        Serial.print("Raw course in degrees = ");
+        Serial.println(gps.course.value());
+        // Course in degrees (double)
+        Serial.print("Course in degrees = ");
+        Serial.println(gps.course.deg());
 
-      // Raw altitude in centimeters (i32)
-      Serial.print("Raw altitude in centimeters = ");
-      Serial.println(gps.altitude.value());
-      // Altitude in meters (double)
-      Serial.print("Altitude in meters = ");
-      Serial.println(gps.altitude.meters());
-      // Altitude in miles (double)
-      Serial.print("Altitude in miles = ");
-      Serial.println(gps.altitude.miles());
-      // Altitude in kilometers (double)
-      Serial.print("Altitude in kilometers = ");
-      Serial.println(gps.altitude.kilometers());
-      // Altitude in feet (double)
-      Serial.print("Altitude in feet = ");
-      Serial.println(gps.altitude.feet());
+        // Raw altitude in centimeters (i32)
+        Serial.print("Raw altitude in centimeters = ");
+        Serial.println(gps.altitude.value());
+        // Altitude in meters (double)
+        Serial.print("Altitude in meters = ");
+        Serial.println(gps.altitude.meters());
+        // Altitude in miles (double)
+        Serial.print("Altitude in miles = ");
+        Serial.println(gps.altitude.miles());
+        // Altitude in kilometers (double)
+        Serial.print("Altitude in kilometers = ");
+        Serial.println(gps.altitude.kilometers());
+        // Altitude in feet (double)
+        Serial.print("Altitude in feet = ");
+        Serial.println(gps.altitude.feet());
 
-      // Number of satellites in use (u32)
-      Serial.print("Number os satellites in use = ");
-      Serial.println(gps.satellites.value());
+        // Number of satellites in use (u32)
+        Serial.print("Number os satellites in use = ");
+        Serial.println(gps.satellites.value());
 
-      // Horizontal Dim. of Precision (100ths-i32)
-      Serial.print("HDOP = ");
-      Serial.println(gps.hdop.value());
-      Serial.println();
+        // Horizontal Dim. of Precision (100ths-i32)
+        Serial.print("HDOP = ");
+        Serial.println(gps.hdop.value());
+        Serial.println();
+      }
     }
   }
 }
